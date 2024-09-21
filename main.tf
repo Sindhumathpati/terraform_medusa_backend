f031a032860128b6d41913d5de2330a1868a7a34
Sindhu Mathapati <sindhumathapati9@gmail.com>
2024-09-21 21:14:56 +0530

    Set up Terraform and GitHub Actions for Medusa deployment

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-west-2" # Change as needed
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "medusa" {
+  ami           = "ami-05134c8ef96964280" # Update with a suitable AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.instance_sg.id]
+
+  tags = {
+    Name = "myserver"
+  }
+}
