dd1d27f266516f44fc21fc901b854f63efa9e652
Sindhu Mathapati <sindhumathapati9@gmail.com>
2024-09-26 12:40:09 +0530

    Update Terraform configuration and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 #output "cluster_name" {
- # value = aws_ecs_cluster.medusa.name
-}#
+# value = aws_ecs_cluster.medusa.name
+#}
 
 #output "task_definition" {
- # value = aws_ecs_task_definition.medusa_task.arn
+# value = aws_ecs_task_definition.medusa_task.arn
 #}
 
 #output "service_name" {
- # value = aws_ecs_service.medusa_service.name
+# value = aws_ecs_service.medusa_service.name
 #}
